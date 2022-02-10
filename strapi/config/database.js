8305ef1af2d682e8c3242cc773dc50fbd95bf970
Vincenzo Russo <v@artetecha.com>
2022-02-10 13:19:12 +0100

    Automatically fall back on SQLite if no PG relationship is found.

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -4,6 +4,7 @@ const path = require('path');
 let dbRelationship = "pg";
 
 // Strapi default sqlite settings.
+let pool = {};
 let connection = {
   connection: {
     client: 'sqlite',
@@ -14,39 +15,42 @@ let connection = {
   },
 };
 
-let pool = {};
-
 if (config.isValidPlatform() && !config.inBuild()) {
   // Platform.sh database configuration.
-  const credentials = config.credentials(dbRelationship);
-  console.log(`Using Platform.sh configuration with relationship ${dbRelationship}.`);
+  try {
+    const credentials = config.credentials(dbRelationship);
+    console.log(`Using Platform.sh configuration with relationship ${dbRelationship}.`);
 
-  pool = {
-    min: 0,
-    max: 10,
-    acquireTimeoutMillis: 600000,
-    createTimeoutMillis: 30000,
-    idleTimeoutMillis: 20000,
-    reapIntervalMillis: 20000,
-    createRetryIntervalMillis: 200,
-  };
+    pool = {
+      min: 0,
+      max: 10,
+      acquireTimeoutMillis: 600000,
+      createTimeoutMillis: 30000,
+      idleTimeoutMillis: 20000,
+      reapIntervalMillis: 20000,
+      createRetryIntervalMillis: 200,
+    };
 
-  connection = {
-    connection: {
-      client: "postgres",
+    connection = {
       connection: {
-        host: credentials.ip,
-        port: credentials.port,
-        database: credentials.path,
-        user: credentials.username,
-        password: credentials.password,
-        ssl: false
+        client: "postgres",
+        connection: {
+          host: credentials.ip,
+          port: credentials.port,
+          database: credentials.path,
+          user: credentials.username,
+          password: credentials.password,
+          ssl: false
+        },
+        debug: false,
+        pool
       },
-      debug: false,
-      pool
-    },
-  };
-
+    };
+  }
+  catch (e) {
+    // Do nothing if 'pg' relationship is not found.
+    // Database configuration will fall back on the SQLite defaults.
+  }
 } else {
   if (config.isValidPlatform()) {
     // Build hook configuration message.
